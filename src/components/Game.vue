8ef21004160940ccb83a00be05cce86cafb42bd7
Brian <bgonz136@gmail.com>
2021-04-17 14:55:11 -0500

    Removed jpg

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="p-2 text-center space-y-2">
-        <img class="h-60 mx-auto" src="../assets/Rock Paper Scissors.jpg" alt="Home Image" />
+        <img class="h-60 mx-auto" src="../assets/home.svg" alt="Home Image" />
         <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">
             <button>Add To Cart</button>
         </div>
     </div>
 </template>
+
+/
