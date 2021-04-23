dbff582b46610166e87b75192857c4c2e0c9cfb3
Brian <bgonz136@gmail.com>
2021-04-22 22:51:28 -0500

    added game library

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,4 +5,39 @@
                 <button>Add To Cart</button>                
             </div>
     </div>
+
+    <div class="p-2 text-center space-y-2"> 
+        <img class="h-60 mx-auto" src="../assets/kirby.jpg" alt="Home Image" />
+            <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">                
+                <button>Add To Cart</button>                
+            </div>
+    </div>
+
+    <div class="p-2 text-center space-y-2"> 
+        <img class="h-60 mx-auto" src="../assets/washDogs.png" alt="Home Image" />
+            <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">                
+                <button>Add To Cart</button>                
+            </div>
+    </div>
+
+    <div class="p-2 text-center space-y-2"> 
+        <img class="h-60 mx-auto" src="../assets/chung.jpg" alt="Home Image" />
+            <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">                
+                <button>Add To Cart</button>                
+            </div>
+    </div>
+
+    <div class="p-2 text-center space-y-2"> 
+        <img class="h-60 mx-auto" src="../assets/hola.jpg" alt="Home Image" />
+            <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">                
+                <button>Add To Cart</button>                
+            </div>
+    </div>
+
+    <div class="p-2 text-center space-y-2"> 
+        <img class="h-60 mx-auto" src="../assets/retail.jpg" alt="Home Image" />
+            <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">                
+                <button>Add To Cart</button>                
+            </div>
+    </div>
 </template>
