c73468764a371c1a60236c87fcc8292db21fd68b
Brian <bgonz136@gmail.com>
2021-04-29 21:05:17 -0500

    styling

--- src/components/RemoveGame.vue
+++ src/components/RemoveGame.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 container mx-auto">
     <div class="p-2 text-center space-y-2">
-        <img class="h-60 mx-auto" src="../assets/home.svg" alt="Home Image" />
+        <img class="h-60 mx-auto" src="../assets/washDogs.png" alt="Home Image" />
             <div class="btn btn-primary btn-sm rounded-btn hover:text-content-100">
                 <button>REMOVE</button>
             </div>
